3c494d7a8588f8a42a7e18e70c0072d2b4e9a173
Shyam Sundar <shyam-11066@shyam-11066.csez.zohocorpin.com>
2021-03-12 20:05:02 +0530

    lint fix

--- src/services/Events.js
+++ src/services/Events.js
@@ -1,8 +1,8 @@
-//get Evens by calendar name and date
+// get Evens by calendar name and date
 
 const getAccessToken = async (code) => {
-  let token = await fetch(
-    `${process.env.REACT_APP_BACKEND_URL}/authorization/${code}/${process.env.REACT_APP_CLIENT_ID}/${process.env.REACT_APP_CLIENT_SECRET}`
+  const token = await fetch(
+    `${process.env.REACT_APP_BACKEND_URL}/authorization/${code}/${process.env.REACT_APP_CLIENT_ID}/${process.env.REACT_APP_CLIENT_SECRET}`,
   )
     .then((res) => res.text());
 
@@ -10,17 +10,16 @@ const getAccessToken = async (code) => {
 };
 
 const getEvents = async (token, cid) => {
-  let result = await fetch(
-    `${process.env.REACT_APP_BACKEND_URL}/getEventList/${token}/${cid}`
+  const result = await fetch(
+    `${process.env.REACT_APP_BACKEND_URL}/getEventList/${token}/${cid}`,
   ).then((res) => res.json());
-  console.log(result);
   return result.events;
 };
 
 const getCalendarId = async (token, name) => {
-  let result = await fetch(`${process.env.REACT_APP_BACKEND_URL}/getCalendarName/${token}/${name}`).then(res => res.json()).catch(err => 0);
+  const result = await fetch(`${process.env.REACT_APP_BACKEND_URL}/getCalendarName/${token}/${name}`).then((res) => res.json()).catch(() => 0);
 
   return result;
-}
+};
 
 export { getEvents, getAccessToken, getCalendarId };
