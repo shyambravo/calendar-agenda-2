1fec84529b28e69a7e6d91c0f1cc5bd51e448768
Shyam Sundar <shyam-11066@shyam-11066.csez.zohocorpin.com>
2021-03-13 00:22:11 +0530

    complete lint setup

--- src/store/calendar.js
+++ src/store/calendar.js
@@ -1,13 +1,14 @@
-import { CalendarModel } from "../models/events/CalendarModel";
+import CalendarModel from '../models/calendar/CalendarModel';
 
 class CalendarStore {
   constructor(data) {
     this.calendar = new CalendarModel({ uid: data.uid, name: data.name });
   }
+
   changeCalendar = (uid, name) => {
     this.calendar.uid = uid;
     this.calendar.anme = name;
   };
 }
 
-export { CalendarStore };
+export default CalendarStore;
