b9d5e4a81fe335a252a325c8c055fc5c0d14b071
Shyam Sundar <shyam-11066@shyam-11066.csez.zohocorpin.com>
2021-03-23 15:41:55 +0530

    width seperation

--- src/pages/home/DayView.js
+++ src/pages/home/DayView.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 import React, { Component } from 'react';
 import './Home.css';
 import Card from '@material-ui/core/Card';
@@ -10,6 +11,9 @@ export default class DayView extends Component {
       day: [],
     };
     this.storePosition = this.storePosition.bind(this);
+    this.sortByDate = this.sortByDate.bind(this);
+    this.findConflict = this.findConflict.bind(this);
+    // this.setWidthAndLeft = this.setWidthAndLeft.bind(this);
   }
 
   componentDidMount() {
@@ -47,19 +51,72 @@ export default class DayView extends Component {
       const minutes = moment(e.fromDate, 'dddd, MMMM Do YYYY, h:mm:ss a').format('mm');
       const time = parseInt(hour * 60, 10) + parseInt(minutes, 10);
       const endHour = moment(e.toDate, 'dddd, MMMM Do YYYY, h:mm:ss a').format('HH');
-      const endMinues = moment(e.toDate, 'dddd, MMMM Do YYYY, h:mm:ss a').format('mm');
-      const height = parseInt(endHour * 60, 10) + parseInt(endMinues, 10);
+      const endMinutes = moment(e.toDate, 'dddd, MMMM Do YYYY, h:mm:ss a').format('mm');
+      const height = parseInt(endHour * 60, 10) + parseInt(endMinutes, 10);
+      const total = (height - time);
       const obj = {
+        startTime: time,
+        endTime: height,
         top: `${time}px`,
         left: 0,
         title: e.title,
         height: `${height - time}px`,
+        totalTime: total,
+        index: 0,
+        width: 100,
       };
       temp.push(obj);
     });
+    this.sortByDate(temp);
     // eslint-disable-next-line react/no-did-update-set-state
     this.setState({
       events: temp,
+    });
+  }
+
+  sortByDate = (arr) => {
+    arr.sort((a, b) => {
+      if (a.startTime === b.startTime) {
+        return b.totalTime - a.totalTime;
+      }
+      return a.startTime - b.startTime;
+    });
+    this.findConflict(arr);
+  }
+
+  findConflict = (arr) => {
+    // const map = {};
+    // for (let i = 0; i < arr.length; i++) {
+    //   map[`${i}`] = [];
+    // }
+    for (let index = 1; index < arr.length; index += 1) {
+      let count = 0;
+      for (let i = 0; i < index; i += 1) {
+        if (arr[index].startTime >= arr[i].startTime && arr[index].startTime <= arr[i].endTime) {
+          count += 1;
+          arr[i].index = count;
+        }
+      }
+
+      count += 1;
+      arr[index].index = count;
+      let width = 100;
+      for (let i = 0; i < arr.length; i += 1) {
+        const temp = width / count;
+        if (arr[i].index !== 0) {
+          if (arr[i].width < temp) {
+            width = (100 - arr[i].width);
+            count -= 1;
+          } else {
+            arr[i].width = temp;
+            arr[i].left = parseInt(parseInt(arr[i].index - 1, 10) * temp, 10);
+          }
+          arr[i].index = 0;
+        }
+      }
+    }
+    this.setState({
+      events: arr,
     });
   }
 
@@ -70,7 +127,12 @@ export default class DayView extends Component {
         <div className="absolute-container">
           {
             events && events.map((event) => (
-              <div className="grid-absolute" style={{ top: event.top, height: event.height }}>
+              <div
+                className="grid-absolute"
+                style={{
+                  top: event.top, height: event.height, width: `${event.width}%`, left: `${event.left}%`,
+                }}
+              >
                 <p>{event.title}</p>
               </div>
             ))
