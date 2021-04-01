c35968054a732c6f81016807165f27e263e0de8a
Shyam Sundar <shyam-11066@shyam-11066.csez.zohocorpin.com>
2021-04-01 11:57:25 +0530

    style fix

--- src/pages/home/MonthView.js
+++ src/pages/home/MonthView.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/no-array-index-key */
 import React from 'react';
 import moment from 'moment';
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 import './Home.css';
 
 export default function MonthView(props) {
@@ -17,33 +18,30 @@ export default function MonthView(props) {
                 </div>
                 <div className="agenda-data">
                   <div className="agenda-duration">
-                    <ul>
+                    <ul className="adenda-content">
                       {eventList[e].map((time, index2) => (
                         <li className="time-list" key={`time ${index2}`}>
-                          <p>
-                            {`${moment(
-                              time.fromDate,
-                              'dddd, MMMM Do YYYY, h:mm:ss a',
-                            ).format('DD-MMM')} (${moment(
-                              time.fromDate,
-                              'dddd, MMMM Do YYYY, h:mm:ss a',
-                            ).format('HH:mm')}) - ${moment(
-                              time.toDate,
-                              'dddd, MMMM Do YYYY, h:mm:ss a',
-                            ).format('DD-MMM')} (${moment(
-                              time.toDate,
-                              'dddd, MMMM Do YYYY, h:mm:ss a',
-                            ).format('HH:mm')})`}
-                          </p>
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                  <div className="agenda-title">
-                    <ul className="agenda-title-list">
-                      {eventList[e].map((title, index2) => (
-                        <li className="title-list">
-                          <p key={`title ${index2}`}>{title.title}</p>
+                          <div className="agenda-date-expand">
+                            <FiberManualRecordIcon />
+                            <p className="agenda-date-content">
+                              {`${moment(
+                                time.fromDate,
+                                'dddd, MMMM Do YYYY, h:mm:ss a',
+                              ).format('DD-MMM')} (${moment(
+                                time.fromDate,
+                                'dddd, MMMM Do YYYY, h:mm:ss a',
+                              ).format('HH:mm')}) - ${moment(
+                                time.toDate,
+                                'dddd, MMMM Do YYYY, h:mm:ss a',
+                              ).format('DD-MMM')} (${moment(
+                                time.toDate,
+                                'dddd, MMMM Do YYYY, h:mm:ss a',
+                              ).format('HH:mm')})`}
+                            </p>
+                          </div>
+                          <div className="agenda-title">
+                            <p key={`title ${index2}`}>{time.title}</p>
+                          </div>
                         </li>
                       ))}
                     </ul>
